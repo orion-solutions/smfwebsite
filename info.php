33cd93af2623070df907c8597e032e0e2a327876
Macky <30364365+SMF-Macky@users.noreply.github.com>
2017-08-10 16:29:18 -0400

    oihspoihfliqwjebflkjsbfkjqwb

--- info.php
+++ info.php
@@ -3,8 +3,8 @@ $host_name = 'db694202085.db.1and1.com';
 $database = 'db694202085';
 $user_name = 'dbo694202085';
 $password = 'ruchunUt7e$h';
-$connect = mysql_connect($host_name, $user_name, $password, $database);
 $sql = "SELECT * FROM `Newsletter`";
+$connect = mysql_connect($host_name, $user_name, $password, $database);
 if (mysql_errno()) {
     die('<p>Failed to connect to MySQL: '.mysql_error().'</p>');
 } else {
