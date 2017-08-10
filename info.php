4e0abaf336fa10616abfb1a989243e15b72da8d9
Macky <30364365+SMF-Macky@users.noreply.github.com>
2017-08-10 16:34:10 -0400

    PHP is fucking retarded

--- info.php
+++ info.php
@@ -3,9 +3,11 @@ $host_name = 'db694202085.db.1and1.com';
 $database = 'db694202085';
 $user_name = 'dbo694202085';
 $password = 'ruchunUt7e$h';
+$connect = mysql_connect($host_name, $user_name, $password, $database);
 $sql = "SELECT * FROM `Newsletter`";
 if (mysql_errno()) {
     die('<p>Failed to connect to MySQL: '.mysql_error().'</p>');
 } else {
     echo '<p>Connection to MySQL server successfully established.</p >';
 }
+?>
