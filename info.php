334202196b425e0dac14c77819338a24fc1ea2d8
Macky <30364365+SMF-Macky@users.noreply.github.com>
2017-08-10 17:02:45 -0400

    Minute changes

--- info.php
+++ info.php
@@ -2,12 +2,13 @@
 $host_name = 'db694202085.db.1and1.com';
 $database = 'db694202085';
 $user_name = 'dbo694202085';
-$password = 'ruchunUt7e$h';
-$connect = mysql_connect($host_name, $user_name, $password, $database);
+$password = '<ruchunUt7e$h>';
+
+$connect = (db694202085.db.1and1.com, dbo694202085, ruchunUt7e$h, db694202085);
 $sql = "SELECT * FROM `Newsletter`";
 if (mysql_errno()) {
-    die('<p>Failed to connect to MySQL: '.mysql_error().'</p>');
+    die('<p>Failed to connect to the server! Please try again!: '.mysql_error().'</p>');
 } else {
-    echo '<p>Connection to MySQL server successfully established.</p >';
+    echo '<p>Communication details received! Thank you for signing up for our newsletter!</p >';
 }
 ?>
