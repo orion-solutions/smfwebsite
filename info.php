3c0d32a6e3e0ff69811c6c0f6e03928001e62b63
SMF-Macky <scientificgames@icloud.com>
2017-08-11 00:09:32 -0400

    Changes to Landing files and Info.php

--- info.php
+++ info.php
@@ -3,12 +3,13 @@ $host_name = 'db694202085.db.1and1.com';
 $database = 'db694202085';
 $user_name = 'dbo694202085';
 $password = '<ruchunUt7e$h>';
-
-$connect = (db694202085.db.1and1.com, dbo694202085, ruchunUt7e$h, db694202085);
+$connect = ('db694202085.db.1and1.com, dbo694202085, ruchunUt7e$h, db694202085');
 $sql = "SELECT * FROM `Newsletter`";
-if (mysql_errno()) {
-    die('<p>Failed to connect to the server! Please try again!: '.mysql_error().'</p>');
-} else {
-    echo '<p>Communication details received! Thank you for signing up for our newsletter!</p >';
-}
+$sql = "SELECT `id`, `name`, `email` FROM `Newsletter` WHERE 1";
+$sql = "INSERT INTO `Newsletter`(`id`, `name`, `email`) VALUES ([value-1],[value-2],[value-3])";
+?>
+<?php
+	
+	header( 'Location: http://www.smfreelancing.com/confirmation.html' ) ;
+
 ?>
